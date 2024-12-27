140017e0828b7caa873233d3f745bc293a612ab3
Abdelghani Adel <a.adel@rockettravelsystem.com>
2024-12-27 13:31:33 +0200

    Base structure for courses

--- app/(with-layout)/courses/page.tsx
+++ app/(with-layout)/courses/page.tsx
@@ -1,14 +1,60 @@
-import React from "react";
-import { baseMetadata } from "@/utils/baseMetadata";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "I Learna Courses",
-  ...baseMetadata,
-};
+"use client";
+import React, { useEffect, useState } from "react";
+import Pagination from "@/components/ui/Pagination";
 
 const Page = () => {
-  return <div>Courses</div>;
+  const [items, setItems] = useState<any[]>([]);
+  const [totalItems, setTotalItems] = useState<number>(100);
+  const [itemsPerPage, setItemsPerPage] = useState<number>(9);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [search, setSearch] = useState<string>("");
+  const [sortBy, setSortBy] = useState<string>("");
+  const [filters, setFilters] = useState<any[]>([]);
+  const [nextPaginateFrom, setNextPaginateFrom] = useState<number>();
+  const [nextPaginateTo, setNextPaginateTo] = useState<number>(0);
+
+  useEffect(() => {
+    fetchItems();
+
+    // update the items
+
+    // update the total items
+
+    // update the current page
+  }, [search, sortBy, filters, nextPaginateFrom, nextPaginateTo]);
+
+  useEffect(() => {
+    setNextPaginateFrom(currentPage * itemsPerPage);
+    setNextPaginateTo(currentPage * itemsPerPage + itemsPerPage);
+  }, [currentPage]);
+
+  const fetchItems = async () => {
+    const reqBody = {
+      search,
+      sortBy,
+      filters,
+      paginateFrom: nextPaginateFrom,
+      paginateTo: nextPaginateTo,
+    };
+
+    console.log(reqBody);
+  };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div>
+      Courses
+      <Pagination
+        currentPage={currentPage}
+        totalItems={totalItems}
+        itemsPerPage={itemsPerPage}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
 };
 
 export default Page;
---
+++ services/coursesService.ts
@@ -0,0 +1,9 @@
+export async function getRecordedCourses(reqBody: any) {}
+
+export async function getLiveCourses() {}
+
+export async function getRecordedCourseDetails() {}
+
+export async function getLiveCourseDetails() {}
+
+export async function getCourseFilters() {}
