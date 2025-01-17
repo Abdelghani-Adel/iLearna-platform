a00a16dd7b09e3a580a1b422d1eb2aec314c3b6c
Abdelghani Adel <a.adel@rockettravelsystem.com>
2025-01-17 16:48:43 +0200

    New routes | Structure for userServices

---
+++ services/userServices.ts
@@ -0,0 +1,13 @@
+export async function getMyCourses() {}
+
+export async function getRecommendedCourses() {}
+
+export async function getCartInfo() {}
+
+export async function checkout() {}
+
+export async function getWishlist() {}
+
+export async function addToWishlist() {}
+
+export async function deleteFromWishlist() {}
