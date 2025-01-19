79e4179ba9ced96b0b3d60fb9f4f4aa2df9b564f
Abdelghani Adel <a.adel@rockettravelsystem.com>
2025-01-19 11:41:05 +0200

    header profile actions

---
+++ components/layout/MainHeaderActions.tsx
@@ -0,0 +1,80 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { FC, useEffect, useState } from "react";
+import { useDetectClickOutside } from "react-detect-click-outside";
+
+const MainHeaderActions = () => {
+  return (
+    <div>
+      <ProfileActions />
+    </div>
+  );
+};
+
+const ProfileActions: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+  const closeMenu = () => setIsOpen(false);
+
+  useEffect(closeMenu, [pathname]);
+
+  const clickOutsideRef = useDetectClickOutside({ onTriggered: closeMenu });
+
+  return (
+    <div>
+      <button onClick={toggleMenu}>
+        <Image
+          width={1024}
+          height={1024}
+          src="/images/avatar.jfif"
+          alt="user avatar"
+          className="w-8 rounded-full"
+        />
+      </button>
+
+      {isOpen && (
+        <div
+          className="absolute top-[105%] right-5 drop-shadow-lg w-80 bg-white rounded-md"
+          ref={clickOutsideRef}
+        >
+          <div className="p-4 border-b">
+            <p className="text-accent text-sm font-semibold">Hello,</p>
+            <p className="text-sm font-semibold text-primary">Ahmed Mohamed</p>
+
+            <button className="w-full bg-accent text-white mt-3 py-2 rounded-md">
+              <Link href="/profile">View and Edit Your Profile</Link>
+            </button>
+          </div>
+
+          <div className="p-4 border-b text-sm text-primary font-normal">
+            <Link href="/my-courses">My Courses</Link>
+          </div>
+          <div className="p-4 border-b text-sm text-primary font-normal">
+            <Link href="/cart">My Cart</Link>
+          </div>
+          <div className="p-4 border-b text-sm text-primary font-normal">
+            <Link href="/wishlist">Wishlist</Link>
+          </div>
+          <div className="p-4 border-b text-sm text-primary font-normal">
+            <Link href="#">Payment Methods</Link>
+          </div>
+          <div className="p-4 border-b text-sm text-primary font-normal">
+            <Link href="#">Become a Freelancer</Link>
+          </div>
+          <div className="p-4 border-b text-sm text-primary font-normal">
+            <Link href="#">Help and Support</Link>
+          </div>
+          <div className="p-4 text-sm text-primary font-normal">
+            <Link href="#">Log out</Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MainHeaderActions;
