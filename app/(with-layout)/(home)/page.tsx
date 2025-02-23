fad51b98c6f77b60f7d81e98122e6efbf5b72f87
Abdelghani Adel <a.adel@rockettravelsystem.com>
2025-02-23 12:16:19 +0200

    Home page

--- app/(with-layout)/(home)/page.tsx
+++ app/(with-layout)/(home)/page.tsx
@@ -10,6 +10,11 @@ import { v4 } from "uuid";
 import SectionTitle from "@/components/ui/SectionTitle";
 import ReviewMessage from "@/components/molecules/ReviewMessage";
 import homecarousel from "@/public/data/homeCarousel.json";
+import { FaPersonChalkboard } from "react-icons/fa6";
+import { SiMinds } from "react-icons/si";
+import { GiBrain } from "react-icons/gi";
+import { HiOutlineRocketLaunch } from "react-icons/hi2";
+import { SlBookOpen } from "react-icons/sl";
 
 export const metadata: Metadata = {
   title: "I Learna",
@@ -73,16 +78,69 @@ export default function Home() {
         </Carousel>
       </section>
 
-      {/* <section className="px-10">
-        <div className="lg:w-1/3">
-          <PageTitle>
-            Unlock Your Potential with Expert-Led Courses & Learning Paths
-          </PageTitle>
+      <section className="2xl:flex 2xl:gap-5">
+        <div className="bg-accent p-10 text-white 2xl:w-2/5 shrink-0 flex flex-col justify-center gap-3">
+          <h2 className="text-4xl font-semibold">
+            Your path to success starts here
+          </h2>
+          <p>
+            Unlock the tools, knowledge, and guidance you need to achieve your
+            goals. With expert-led courses, personalized learning paths, and a
+            seamless platform experience, we’re here to help you take the next
+            step toward success
+          </p>
         </div>
-      </section> */}
+
+        <div className="grid grid-cols-1 grid-rows-4 gap-5 p-5 2xl:grid-cols-2 2xl:grid-rows-2">
+          <div>
+            <div className="text-4xl bg-[#EBD78E4D] w-max p-3 mb-2 rounded-full">
+              <FaPersonChalkboard className="text-[#EBD78E]" />
+            </div>
+            <h2 className="text-xl font-semibold mb-2">Expert-Led Learning</h2>
+            <p className="text-customGray">
+              Gain insights from top professionals with real-world experience,
+              ensuring you receive practical and up-to-date knowledge
+            </p>
+          </div>
+          <div>
+            <div className="text-4xl bg-[#38B00033] w-max p-3 mb-2 rounded-full">
+              <GiBrain className="text-[#5DB87E]" />
+            </div>
+            <h2 className="text-xl font-semibold mb-2">
+              Personlized Learning Paths
+            </h2>
+            <p className="text-customGray">
+              Choose tracks, courses, or live sessions tailored to your goals,
+              with flexible options for individual or group learning
+            </p>
+          </div>
+          <div>
+            <div className="text-4xl bg-[#43B1E433] w-max p-3 mb-2 rounded-full">
+              <HiOutlineRocketLaunch className="text-[#43B1E4]" />
+            </div>
+            <h2 className="text-xl font-semibold mb-2">
+              Seamless Learning Experience
+            </h2>
+            <p className="text-customGray">
+              Enjoy a user-friendly platform with secure access, progress
+              tracking, and expert support to help you succeed at your own pace
+            </p>
+          </div>
+          <div>
+            <div className="text-4xl bg-[#E7A3A333] w-max p-3 mb-2 rounded-full">
+              <SlBookOpen className="text-[#E18C8C]" />
+            </div>
+            <h2 className="text-xl font-semibold mb-2">Hands-On Experience</h2>
+            <p className="text-customGray">
+              Engage in real-world case studies, interactive sessions, and
+              step-by-step guidance to master industry-relevant skills
+            </p>
+          </div>
+        </div>
+      </section>
 
       <section>
-        <div className="max-w-sm ml-20">
+        <div className="ml-5 2xl:max-w-sm 2xl:ml-20">
           <SectionTitle>What our users say about iLearna</SectionTitle>
         </div>
         <Carousel settings={carouselSettings}>
@@ -94,7 +152,7 @@ export default function Home() {
         </Carousel>
       </section>
 
-      <section className="flex justify-center p-10">
+      <section className="2xl:flex 2xl:justify-center">
         <Faq />
       </section>
     </main>
